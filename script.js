b7c1d1cba2ce0a59ddb8fe84380ac8c7a2e0eddb
cadltop <158861368+cadltop@users.noreply.github.com>
2024-03-02 23:50:11 -0500

    Add code to getComputerChoice function.
    The code within the function allow the computer to choose a value between 1 and
    3 randomly and based on that value the function can asign the value of the variable
    'selection' with a switch statement using the value of the constants.

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const ROCK = "Rock";
 const PAPER = "Paper";
-const SCISSORS = "Scissors";;
+const SCISSORS = "Scissors";
 
 function getComputerChoice() {
-    
+    let selection, 
+    randomNumber = Math.round(Math.random() * 3 + 1);
+
+    switch (randomNumber) {
+        case 1:
+            selection = ROCK;
+            break;
+        case 2:
+            selection = PAPER;
+            break;
+        case 3:
+            selection = SCISSORS;
+            break;
+    }
+
+    return selection;
 }

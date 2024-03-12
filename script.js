0ad9f5c022a9b2489da15bd447cc715e7276a35c
cadltop <158861368+cadltop@users.noreply.github.com>
2024-03-12 12:40:15 -0500

    Add code to manipulate new .container.

--- script.js
+++ script.js
@@ -108,16 +108,16 @@ function playGame() {
             const messageP = document.querySelector('.message');
             messageP.textContent = '';
             
+            if (playerCounter === 5 && computerCounter == 5) {
+                document.querySelector('.message').innerHTML = "It's a tie!";
+                playerCounter = 0; computerCounter = 0;
+            }
             if (playerCounter === 5) {
                 document.querySelector('.message').innerHTML = 'You Won!';
                 playerCounter = 0; computerCounter = 0;
             }
             if (computerCounter === 5) {
                 document.querySelector('.message').innerHTML = 'You Loss!';
-                playerCounter = 0; computerCounter = 0;
-            }
-            if (playerCounter === 5 && computerCounter == 5) {
-                document.querySelector('.message').innerHTML = "It's a tie!";
                 playerCounter = 0; computerCounter = 0;
             }
             
